35c333fabe596714077c706c449f8d4079d5565b
SJShaw <nomis@internode.on.net>
2013-06-05 06:27:28 +1000

    the database support for !when

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer,String
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer,String, Float
 import os
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -10,5 +10,11 @@ quotes = Table('quotes', metadata,
     Column('id', Integer, primary_key=True),
     Column('body', String(200))
 )
+times = Table('times', metadata,
+    Column('name', String(20), primary_key=True),
+    Column('time', Float)
+)
+
 if __name__ == '__main__':
-	quotes.create()
+    quotes.create()
+    times.create()
