52a499a6c41db20c75b33cd4b932838061ce9a93
Jeremy Smitherman <jeremysmitherman@gmail.com>
2013-12-06 18:58:21 +0000

    Removed new command from cbb branch

--- actions.py
+++ actions.py
@@ -535,7 +535,6 @@ class Commander(object):
         '!thanks' : thanks,
         '!ron': ron,
         '!bolt': lightning,
-        '!new': new,
         '!when': when,
         '!settime':settime,
         '!ud': urbandictionary,
